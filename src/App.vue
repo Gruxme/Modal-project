ed9d6e563563ed43cc297c9dd7b2d82751d28f6c
abiari <abiari@student.1337.ma>
2022-03-21 17:00:21 +0100

    Modal-project finished

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,29 @@
   <img alt="Vue logo" src="./assets/logo.png">
   <br>
   <h1>{{ title }}</h1>
-  <Modal />
+  <p>Welcome...</p>
+  <teleport to="#modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Sign Up!</h1>
+      <p>Grab your merch now!</p>
+    </Modal>
+  </teleport>
+  <teleport to="#modals" v-if="showModalTwo">
+    <Modal @close="toggleModalTwo">
+       <template v-slot:links>
+        <a href="#">Sign In</a>
+        <a href="#">More Info</a>
+      </template>
+      <h1>Sign In and get your free ticket!</h1>
+      <p>*One month free subscription ticket</p>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">Open Modal</button>
+  <button @click="toggleModalTwo">Open Modal Two</button>
 </template>
 
 <script>
@@ -14,14 +36,24 @@ export default {
   },
   data() {
     return {
-      title: 'My first Vue app'
+      title: 'My first Vue app',
+      showModal: false,
+      showModalTwo: false
     };
+  },
+  methods: {
+    toggleModal(){
+      this.showModal = !this.showModal;
+    },
+    toggleModalTwo(){
+      this.showModalTwo = !this.showModalTwo;
+    }
   }
 }
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
